f7df6016db40aad7061a5d8f605188c835662a6e
Luna <luna.realm.io.bennett24@outlook.com>
2024-10-20 13:45:05 -0700

    Update kernelsu_image_resizer.c

--- utilites/kernelsu_image_resizer.c
+++ utilites/kernelsu_image_resizer.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 int main() {
     char *args[] = {"e2fsck", "-f", "/data/adb/ksu/modules_update.img", NULL};
     char *args_two[] = {"resize2fs", "-f", "/data/adb/ksu/modules_update.img", "500M", NULL};
+
+    // Fork for e2fsck
     pid_t pid1 = fork();
     if (pid1 == 0) {
         execvp(args[0], args);
+        exit(1);
     }
-    wait(NULL);
-    pid_t pid2 = fork();
-    if (pid2 == 0) {
-        execvp(args_two[0], args_two);
+
+    // Fork for resize2fs, but only if we are in the parent process
+    if (pid1 > 0) {
+        pid_t pid2 = fork();
+        if (pid2 == 0) {
+            execvp(args_two[0], args_two);
+            exit(1);
+        }
     }
-    wait(NULL);
+
+    // Parent process returns immediately
     return 0;
 }
