88dd63b918e133c22b21b4da983291c4dcbd2ad7
Luna <luna.realm.io.bennett24@outlook.com>
2024-11-03 03:53:18 -0800

    Create let-go.sh
    thanks @gawasvedraj for this script.
    
    Co-Authored-By: Vedraj Gawas <82878564+gawasvedraj@users.noreply.github.com>

---
+++ test-device-status/let-go.sh
@@ -0,0 +1,40 @@
+if [[ ! -f "./dependencies/util_functions.sh" ]]; then
+  echo -e "[\e[0;35m$(date +%d-%m-%Y) \e[0;37m- \e[0;32m$(date +%H:%M%p)] [:\e[0;36mABORT\e[0;37m:] -\e[0;31m Can't find functions wrapper script, please try again later...\e[0;37m"
+  sleep 0.5
+  exit 1
+else 
+  source ./dependencies/util_functions.sh
+fi
+
+if [[ ! -f "./dependencies/makeconfigs.prop" ]]; then
+  echo -e "[\e[0;35m$(date +%d-%m-%Y) \e[0;37m- \e[0;32m$(date +%H:%M%p)] [:\e[0;36mABORT\e[0;37m:] -\e[0;31m Can't find makeconfigs file, please try again later...\e[0;37m"
+  sleep 0.5
+  exit 1
+else 
+  source ./dependencies/makeconfigs.prop
+fi
+
+n=0
+
+if strings ${SYSTEM_DIR}/framework/framework.jar | grep "onEngineGetCertificateChain"; then
+	console_print "onEngineGetCertificateChain detected!!!!"
+	n="$((n + 1))"
+fi
+
+if strings ${SYSTEM_DIR}/framework/framework.jar | grep "PixelPropsUtils"; then
+	console_print "PixelPropsUtils detected!!!!"
+	n="$((n + 1))"
+fi
+
+if find ${SYSTEM_DIR}/ -iname "*lineage*" 2>/dev/null; then
+	console_print "LineageOS detected!!!!"
+	n="$((n + 1))"
+fi
+
+if [[ "${n}" -ge "1" ]]; then
+	console_print "Modifications on the environment has been detected!!!!!!"
+else
+	console_print "System image haven't been modified."
+fi
+
+console_print "Special Thanks to gawas vedraj from telegram!!!!!!!"
