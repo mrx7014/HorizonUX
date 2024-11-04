d7d89531347f5dd7cb324f71402a5c35a0289d00
Luna <luna.realm.io.bennett24@outlook.com>
2024-11-04 05:53:15 -0800

    move some stuffs to the util-functions and add some junk codes to fill up my needs.

--- dependencies/util_functions.sh
+++ dependencies/util_functions.sh
@@ -263,3 +263,115 @@ function ask() {
     return 1
   fi
 }
+
+function initialize_macloader() {
+    local line
+    local macloader_array=(
+        "service macloader /vendor/bin/hw/macloader"
+        "    class main"
+        "    user wifi"
+        "    group wifi inet net_raw net_admin"
+        "    capabilities NET_ADMIN NET_RAW SYS_MODULE"
+        "    oneshot"
+        "# end of WiFi MFG (TestMode)"
+    )
+
+    # Add the macloader lines to the file
+    for line in "${macloader_array[@]}"; do
+        echo "$line" >> ./build/vendor/etc/init/wifi.rc
+    done
+}
+
+function remove_attributes() {
+	local INPUT_FILE="$1"
+	local OUTPUT_FILE="$2"
+	local NAME_TO_SKIP="$3"
+
+	# Start writing the new XML file
+	{
+		echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+		echo "<manifest version=\"2.0\" type=\"device\" target-level=\"3\">"
+		
+		# Read each line of the input XML
+		while IFS= read -r line; do
+			# Check if the line starts a <hal> block
+			if [[ "$line" == *"<hal"* ]]; then
+				# Read the entire <hal> block into a variable
+				block="$line"
+				skip_block=false
+
+				# Check subsequent lines for the closing </hal>
+				while IFS= read -r line; do
+					block+=$'\n'"$line"
+					if [[ "$line" == *"</hal>"* ]]; then
+						break
+					fi
+				done
+				
+				# Check if the block contains the <name> to skip
+				if [[ "$block" == *"<name>$NAME_TO_SKIP</name>"* ]]; then
+					# Skip this block
+					continue
+				fi
+				
+				# Write the entire block to the output file if not skipped
+				echo "$block"
+			else
+				# Write lines that are not part of a <hal> block
+				echo "$line"
+			fi
+		done < "$INPUT_FILE"
+		
+		echo "</manifest>"
+	} > "$OUTPUT_FILE"
+
+	# Feedback
+	echo "Rewritten XML saved to $OUTPUT_FILE, skipping <hal> with name='$NAME_TO_SKIP'."
+}
+
+function nuke_stuffs() {
+    local service
+    local line
+	local tlc
+	local shit
+	local stuffs2nukeinvintf=(
+        "android.hardware.dumpstate*.xml"
+        "engmode_manifest.xml"
+		"vaultkeeper_manifest.xml"
+        "vendor.samsung.hardware.tlc*.xml"
+        "wsm_manifest.xml"
+    )
+	local stuffs2nukeininitdir=(
+		"${VENDOR_ETC_FOLDER}/init/*android.hardware.dumpstate@*.rc"
+		"${VENDOR_ETC_FOLDER}/init/boringssl_self_test.rc"
+		"${VENDOR_ETC_FOLDER}/init/cass.rc"
+		"${VENDOR_ETC_FOLDER}/init/vaultkeeper_common.rc"
+		"${VENDOR_ETC_FOLDER}/init/pa_daemon_teegris.rc"
+		"${VENDOR_ETC_FOLDER}/init/wsm-service.rc"
+		"${VENDOR_ETC_FOLDER}/init/vendor.samsung.hardware.tlc*.rc"
+	)
+
+    for service in "security.wsm" "security.proca" "tlc.ucm" "tlc.payment"; do
+		echo -e "\e[0;31m [!] - Removing ${service} service from the system config files..."
+		remove_attributes "${service}"
+	done
+	for line in "${stuffs2nukeinvintf[@]}"; do
+		if [ -f "${VENDOR_ETC_FOLDER}/vintf/manifest/${line}" ]; then
+			rm -f "${VENDOR_ETC_FOLDER}/vintf/manifest/${line}"
+			echo " [!] - Deleting ${line}..."
+		fi
+	done
+	for tlc in "$(ls ${VENDOR_ETC_FOLDER}/init/ | grep tlc.)"; do
+		if [ -f "${tlc}" ]; then
+			rm -f ${tlc}
+			echo " [!] - Deleting ${tlc}..."
+		fi
+	done
+	for shit in "${stuffs2nukeininitdir[@]}"; do
+		if [ -f "${shit}" ]; then
+			rm -f "${shit}"
+			echo -e " [!] - Deleting ${shit}...\e[0m"
+		fi
+	done
+	echo ""
+}
