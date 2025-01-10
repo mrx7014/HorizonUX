828ba0c1c843c8a6482f8d80bf2534c07e29ad93
Luna <luna.realm.io.bennett24@outlook.com>
2025-01-10 07:06:57 -0800

    source_wide: fix include/horizonux.h syntax issues i forgor to fix.

--- include/horizonux.h
+++ include/horizonux.h
@@ -1,4 +1,4 @@
-#ifndef HORIZONUX_H 
+#ifndef HORIZONUX_H
 #define HORIZONUX_H
 
 #include <iostream>
@@ -10,120 +10,104 @@
 #include <ctime>
 #include <algorithm>
 
-using namespace std;
-
 class HorizonUX {
-    private:
-        std::string command;
-        HorizonUX(std::string commandInput) : command(commandInput) {}
+private:
+    std::string command;
+    explicit HorizonUX(const std::string& commandInput) : command(commandInput) {}
 
-    public:
-        static std::pair<std::string, int> ExecuteCommandsInTheHorizonUXSystemConsole(const std::string& command) {
-            FILE* pipe = popen(command.c_str(), "r");
-            if (!pipe) {
-                std::cerr << "Error opening pipe for command: " << command << std::endl;
-                return {"", -1};
-            }
-
-            array<char, 128> buffer;
-            string result;
-            while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
-                result += buffer.data();
-            }
-
-            int exitCode = pclose(pipe);
-            return {result, exitCode};
+public:
+    static std::pair<std::string, int> ExecuteCommandsInTheHorizonUXSystemConsole(const std::string& command) {
+        FILE* pipe = popen(command.c_str(), "r");
+        if (!pipe) {
+            std::cerr << "Error opening pipe for command: " << command << std::endl;
+            return {"", -1};
         }
-
-        bool DoesThisFileEvenExist(const std::string& fileName) {
-            std::ifstream file(fileName);
-            return file.is_open();
+        std::array<char, 128> buffer;
+        std::string result;
+        while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
+            result += buffer.data();
         }
+        int exitCode = pclose(pipe);
+        return {result, exitCode};
+    }
 
-        bool iHaveIdentityCrisisManWTF() {
-            return DoesThisFileEvenExist("/system/bin/resetprop");
+    bool DoesThisFileEvenExist(const std::string& fileName) const {
+        std::ifstream file(fileName);
+        return file.is_open();
+    }
+
+    bool iHaveIdentityCrisisManWTF() const {
+        return DoesThisFileEvenExist("/system/bin/resetprop");
+    }
+
+    bool setSystemProperty(const std::string& propertyVariableName, const std::string& propertyVariableValue) {
+        std::ostringstream baseArgumentStream;
+        if (iHaveIdentityCrisisManWTF()) {
+            baseArgumentStream << "resetprop " << propertyVariableName << " " << propertyVariableValue;
         }
+        else {
+            baseArgumentStream << "setprop " << propertyVariableName << " " << propertyVariableValue;
+        }
+        auto [output, exitCode] = ExecuteCommandsInTheHorizonUXSystemConsole(baseArgumentStream.str());
+        return exitCode == 0;
+    }
+    std::string getSystemProperty(const std::string& propertyVariableName) {
+        std::ostringstream baseArgumentStream;
+        baseArgumentStream << "getprop " << propertyVariableName;
+        auto [output, exitCode] = ExecuteCommandsInTheHorizonUXSystemConsole(baseArgumentStream.str());
+        return output;
+    }
+    std::pair<std::string, int> getSystemSettingsValues(const std::string& SystemTable, const std::string& propertyVariableName) {
+        std::ostringstream baseArgumentStream;
+        if (SystemTable == "system" || SystemTable == "global" || SystemTable == "secure") {
+            baseArgumentStream << "settings get " << SystemTable << " " << propertyVariableName;
+        }
+        auto [output, exitCode] = ExecuteCommandsInTheHorizonUXSystemConsole(baseArgumentStream.str());
+        return {output, exitCode};
+    }
+    bool setSystemSettingsProperty(const std::string& SystemTable, const std::string& propertyVariableName, const std::string& propertyVariableValue) {
+        if (SystemTable != "system" && SystemTable != "global" && SystemTable != "secure") {
+            std::cerr << "Invalid SystemTable: " << SystemTable << std::endl;
+            return false;
+        }
+        std::ostringstream baseArgumentStream;
+        baseArgumentStream << "settings put " << SystemTable << " " << propertyVariableName << " " << propertyVariableValue;
+        auto [output, exitCode] = ExecuteCommandsInTheHorizonUXSystemConsole(baseArgumentStream.str());
+        return exitCode == 0;
+    }
+    void ThrowLogsToATextFile(const std::string& service, const std::string& message, const std::string& the_logfile) {
+        std::string logService = service;
+        std::transform(logService.begin(), logService.end(), logService.begin(), ::toupper);
 
-        bool setSystemProperty(const std::string& propertyVariableName, const std::string& propertyVariableValue) {
-            std::ostringstream baseArgumentStream;
-            if (iHaveIdentityCrisisManWTF()) {
-                baseArgumentStream << "resetprop " << propertyVariableName << " " << propertyVariableValue;
+        if (message.empty()) {
+            std::cerr << " - missing arguments, can't process anything..." << std::endl;
+        }
+        else {
+            std::time_t currentTime = std::time(nullptr);
+            char dateTimeStr[100], timeStr[100];
+            std::strftime(dateTimeStr, sizeof(dateTimeStr), "%d-%m-%Y", std::localtime(&currentTime));
+            std::strftime(timeStr, sizeof(timeStr), "%H:%M%p", std::localtime(&currentTime));
+
+            std::ofstream logFile(the_logfile, std::ios_base::app);
+            if (logFile.is_open()) {
+                logFile << "/ [" << timeStr << "]-[" << dateTimeStr << "] / " << logService << " / " << message << " /" << std::endl;
             }
             else {
-                baseArgumentStream << "setprop " << propertyVariableName << " " << propertyVariableValue;
-            }
-            std::string baseArgument = baseArgumentStream.str();
-            HorizonUX console = HorizonUX::parseTheArguments(baseArgument);
-            auto [output, exitCode] = HorizonUX::ExecuteCommandsInTheHorizonUXSystemConsole(console.command);
-            return exitCode == 0;
-        }
-
-        std::string getSystemProperty(const std::string& propertyVariableName) {
-            std::ostringstream baseArgumentStream;
-            baseArgumentStream << "getprop " << propertyVariableName;
-            HorizonUX console = HorizonUX::parseTheArguments(baseArgumentStream);
-            auto [output, exitCode] = HorizonUX::ExecuteCommandsInTheHorizonUXSystemConsole(console.command);
-            return output;
-        }
-
-        std::pair<std::string, int> getSystemSettingsValues(const std::string& SystemTable, const std::string& propertyVariableName) {
-            std::ostringstream baseArgumentStream;
-            int exitCode = 0;
-            if (SystemTable == "system" || SystemTable == "global" || SystemTable == "secure") {
-                baseArgumentStream << "settings get " << SystemTable << " " << propertyVariableName;
-            }
-            HorizonUX console = HorizonUX::parseTheArguments(baseArgumentStream);
-            auto [output, commandExitCode] = HorizonUX::ExecuteCommandsInTheHorizonUXSystemConsole(console.command);
-            exitCode = commandExitCode;
-            return std::make_pair(output);
-        }
-
-        bool setSystemSettingsProperty(const std::string& SystemTable, const std::string& propertyVariableName, const std::string& propertyVariableValue) {
-            if (SystemTable != "system" && SystemTable != "global" && SystemTable != "secure") {
-                std::cerr << "Invalid SystemTable: " << SystemTable << std::endl;
-                return false;
-            }
-            std::ostringstream baseArgumentStream;
-            baseArgumentStream << "settings put " << SystemTable << " " << propertyVariableName << " " << propertyVariableValue;
-            std::string baseArgument = baseArgumentStream.str();
-            HorizonUX console = HorizonUX::parseTheArguments(baseArgument);
-            auto [output, exitCode] = HorizonUX::ExecuteCommandsInTheHorizonUXSystemConsole(console.command);
-            return exitCode == 0;
-        }
-
-        void ThrowLogsToATextFile(const std::string& service, const std::string& message, const std::string& the_logfile) {
-            std::string logService = service;
-            std::transform(logService.begin(), logService.end(), logService.begin(), ::toupper);
-
-            if (message.empty()) {
-                std::cout << " - missing arguments, can't process anything..." << std::endl;
-            }
-            else {
-                std::time_t currentTime = std::time(nullptr);
-                char dateTimeStr[100];
-                std::strftime(dateTimeStr, sizeof(dateTimeStr), "%d-%m-%Y", std::localtime(&currentTime));
-                char timeStr[100];
-                std::strftime(timeStr, sizeof(timeStr), "%H:%M%p", std::localtime(&currentTime));
-
-                std::ofstream logFile(the_logfile, std::ios_base::app);
-                if (logFile.is_open()) {
-                    logFile << "/ [" << timeStr << "]-[" << dateTimeStr << "] / " << logService << " / " << message << " /" << std::endl;
-                    logFile.close();
-                }
-                else {
-                    std::cerr << "Unable to open log file: " << the_logfile << std::endl;
-                }
+                std::cerr << "Unable to open log file: " << the_logfile << std::endl;
             }
         }
-
-        void HorizonUXLoggerService(const std::string& service, const std::string& message, const std::string& the_logfile) {
-            if (DoesThisFileEvenExist("/data/horizonux/logs/horizon_ishiimi_logfile.log")) {
-                ThrowLogsToATextFile(service, message, "/data/horizonux/logs/horizon_ishiimi_logfile.log");
-            }
-            else {
-                std::cerr << "Can't find logfile, skipping logging..." << std::endl;
-            }
+    }
+    void HorizonUXLoggerService(const std::string& service, const std::string& message, const std::string& the_logfile) {
+        if (DoesThisFileEvenExist("/data/horizonux/logs/horizon_ishiimi_logfile.log")) {
+            ThrowLogsToATextFile(service, message, "/data/horizonux/logs/horizon_ishiimi_logfile.log");
         }
+        else {
+            std::cerr << "Can't find logfile, skipping logging..." << std::endl;
+        }
+    }
+    static HorizonUX parseTheArguments(const std::ostringstream& argumentStream) {
+        return HorizonUX(argumentStream.str());
+    }
 };
 
 #endif
