49e6a38b2409bd97613adb4f04c8fb1cae887464
Luna <luna.realm.io.bennett24@outlook.com>
2025-02-13 20:42:47 +0530

    source_wide: change the bot id in horizon/misc_rom_stuffs/changeToPeakRefreshRateifTheDeviceisInLockscreen.c
    Signed-off-by: Luna <luna.realm.io.bennett24@outlook.com>

--- horizon/misc_rom_stuffs/changeToPeakRefreshRateifTheDeviceisInLockscreen.c
+++ horizon/misc_rom_stuffs/changeToPeakRefreshRateifTheDeviceisInLockscreen.c
@@ -17,7 +17,7 @@ int main() {
             executeCommands("settings put system min_refresh_rate 120");
         } 
         else {
-            printdbg("Weird device refresh rate, please send the log file (located in /sdcard/Horizon/changeToPeakRefreshRateifTheDeviceisInLockscreen.log) to the @luna_bugreporter_bot in Telegram.");
+            printdbg("Weird device refresh rate, please send the log file (located in /sdcard/Horizon/changeToPeakRefreshRateifTheDeviceisInLockscreen.log) to @luna_project_bugreporter_bot in Telegram.");
         }
     }
 }
