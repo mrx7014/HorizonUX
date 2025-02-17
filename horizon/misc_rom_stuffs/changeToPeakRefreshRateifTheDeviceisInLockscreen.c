11b9fbce7933277fa922b2939a534d92dfd7f220
Luna <luna.realm.io.bennett24@outlook.com>
2025-02-17 14:27:12 +0530

    source_wide: change the log path to the local Konsole path

--- horizon/misc_rom_stuffs/changeToPeakRefreshRateifTheDeviceisInLockscreen.c
+++ horizon/misc_rom_stuffs/changeToPeakRefreshRateifTheDeviceisInLockscreen.c
@@ -3,10 +3,14 @@
 
 bool DEBUG_MESSAGES_ARE_ENABLED = false;
 bool WRITE_LOGS_INTO_A_FILE = false;
-//char *LOG4HORIZONFILE = "/mnt/c/Users/Luna/Desktop/tst_log_changeToPeakRefreshRateifTheDeviceisInLockscreen.log";
 char *LOG4HORIZONFILE = "/sdcard/Horizon/changeToPeakRefreshRateifTheDeviceisInLockscreen.log";
 
 int main() {
+    // uhrm what the sigma
+    if(touch("/home/luna/bw") == 0) {
+        LOG4HORIZONFILE = "/mnt/c/Users/Luna/Desktop/tst_log_changeToPeakRefreshRateifTheDeviceisInLockscreen.log";
+        erase_file_content(LOG4HORIZONFILE);
+    }
     // let's clear the logs first.
     executeCommands("logcat -c");
     int MaxRefreshRate = getPeakRefreshRate();
@@ -19,7 +23,7 @@ int main() {
             executeCommands("settings put system peak_refresh_rate 120");
             executeCommands("settings put system min_refresh_rate 120");
         }
-        else if(MaxRefreshRate == -1) {
+        else {
             error_print("Couldn't retrieve the device's peak refresh rate.");
             return 1;
         }
