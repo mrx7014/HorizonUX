7b5bc33d4dea7e3f78c63b39bb6aed20df9edb97
Luna <luna.realm.io.bennett24@outlook.com>
2025-04-12 18:37:17 +0530

    source_wide: Tried to fix dumper function

--- workflow_builder/firmwareDumper.sh
+++ workflow_builder/firmwareDumper.sh
@@ -20,7 +20,6 @@
 TARGET_DEVICE_FULL_FIRMWARE_LINK="$1"
 theBotToken="$2"
 chatID="$3"
-PACK_IMAGE_WITH_TS_FORMAT="$4"
 
 # Paths
 firmwareZip="./local_build/downloads/firmware.zip"
@@ -45,63 +44,56 @@ console_print tg "Firmware Dump started at $(TZ=America/Phoenix date '+%d %b %Y,
 # Download firmware
 console_print "Downloading firmware package..."
 download_stuffs "${TARGET_DEVICE_FULL_FIRMWARE_LINK}" "${firmwareZip}" || abort "Failed to download firmware."
-console_print tg "Firmware package downloaded at $(TZ=America/Phoenix date '+%I:%M%p')"
+console_print tg "Firmware package finished downloading at $(TZ=America/Phoenix date '+%I:%M%p')"
 
 # Unzip AP and HOME_CSC
-for specificTargetFirmwareFile in $(unzip -Z1 "${firmwareZip}" | grep -E 'AP|HOME_CSC'); do
-    console_print tg "Unpacking firmware: ${specificTargetFirmwareFile}"
-    unzip -q "${firmwareZip}" "${specificTargetFirmwareFile}" -d "${extrdDir}" >> "$thisConsoleTempLogFile"
+for specificFirmwareFile in $(unzip -Z1 "${firmwareZip}" | grep -E 'AP|HOME_CSC'); do
+    console_print tg "Unpacking firmware: ${specificFirmwareFile}"
+    unzip -q "${firmwareZip}" "${specificFirmwareFile}" -d "${extrdDir}" >> "$thisConsoleTempLogFile" || abort "Failed to extract $specificFirmwareFile"
 done
-rm -f "${firmwareZip}"
+rm -f "${firmwareZip}" || abort "Failed to delete ${firmwareZip}"
 
 # Find real paths
 HOME_CSC=$(find "${extrdDir}" -type f -name 'HOME_CSC_*.tar.md5' | head -n1)
 AP=$(find "${extrdDir}" -type f -name 'AP*.tar.md5' | head -n1)
-
 [ -f "$HOME_CSC" ] || abort "HOME_CSC archive not found!"
 [ -f "$AP" ] || abort "AP archive not found!"
 
 # Extract HOME_CSC content
 if tar -tf "$HOME_CSC" | grep -q "optics"; then
-    console_print tg "Found prism in HOME_CSC, extracting..."
-    tar -xf "$HOME_CSC" -C "${extrdDir}"
-    extractStuffsByTheirFormatSpecifier "${extrdDir}/prism"* "${cmprsDir}/prism.img" NULL
+    console_print tg "Found optics in HOME_CSC, extracting..."
+    tar -xf "$HOME_CSC" -C "${extrdDir}" --wildcards 'optics*'
+    extractStuffsByTheirFormatSpecifier "${extrdDir}/optics"* "${cmprsDir}/optics.img" NULL
+    zstd -T0 --ultra -22 "${cmprsDir}/optics.img" -o "${cmprsDir}/optics.zst"
+    uploadGivenFileToTelegram "${cmprsDir}/optics.zst" && rm -f "${cmprsDir}/optics.zst"
 elif tar -tf "$HOME_CSC" | grep -q "product"; then
     console_print tg "Found product in HOME_CSC, extracting..."
-    tar -xf "$HOME_CSC" -C "${extrdDir}"
+    tar -xf "$HOME_CSC" -C "${extrdDir}" --wildcards 'product*'
     extractStuffsByTheirFormatSpecifier "${extrdDir}/product"* "${cmprsDir}/product.img" NULL
+    zstd -T0 --ultra -22 "${cmprsDir}/product.img" -o "${cmprsDir}/product.zst"
+    uploadGivenFileToTelegram "${cmprsDir}/product.zst" && rm -f "${cmprsDir}/product.zst"
 fi
 
 # Extract AP content
 if tar -tf "$AP" | grep -q "super"; then
-    console_print "Super image found, extracting..."
-    tar -xf "$AP" -C "${extrdDir}"
+    console_print "Found super in AP, extracting..."
+    tar -xf "$AP" -C "${extrdDir}" --wildcards 'super*'
     extractStuffsByTheirFormatSpecifier "${extrdDir}/super"* "${cmprsDir}/super.img" NULL
+    zstd -T0 --ultra -22 "${cmprsDir}/super.img" -o "${cmprsDir}/super.zst"
+    uploadGivenFileToTelegram "${cmprsDir}/super.zst" && rm -f "${cmprsDir}/super.zst"
 elif tar -tf "$AP" | grep -q "system"; then
-    console_print tg "Found system in AP, extracting..."
     for img in system vendor; do
-        tar -xvf "$AP" "$img" -C "${extrdDir}"
-        extractStuffsByTheirFormatSpecifier "${extrdDir}/${img}"* "${cmprsDir}/${img}.img" NULL
-        rm -f "${extrdDir}/${img}"*
+        if tar -tf "$AP" | grep -q "$img"; then
+            console_print "Found ${img} in AP, extracting..."
+            tar -xf "$AP" -C "${extrdDir}" --wildcards "${img}*"
+            extractStuffsByTheirFormatSpecifier "${extrdDir}/${img}"* "${cmprsDir}/${img}.img" NULL
+            zstd -T0 --ultra -22 "${cmprsDir}/${img}.img" -o "${cmprsDir}/${img}.zst"
+            uploadGivenFileToTelegram "${cmprsDir}/${img}.zst" && rm -f "${cmprsDir}/${img}.zst"
+            rm -f "${extrdDir}/${img}"*
+        fi
     done
 fi
 
-# Pack images
-console_print "Packing and uploading to Telegram..."
-TS_TAR="${cmprsDir}/tsFirmware.tar"
-
-for unpacked in "${cmprsDir}"/*.img; do
-    console_print tg "Packing ${unpacked}..."
-    tar --append --file="${TS_TAR}" -C "${cmprsDir}" "$(basename "${unpacked}")"
-    rm -f "${unpacked}"
-done
-
-# Compress with zstd if needed
-if [[ "${PACK_IMAGE_WITH_TS_FORMAT}" == "zstd" ]]; then
-    zstd -T0 --ultra -22 "${TS_TAR}" -o "${cmprsDir}/tsFirmware.zst"
-    uploadGivenFileToTelegram "${cmprsDir}/tsFirmware.zst" && rm -f "${cmprsDir}/tsFirmware.zst"
-else
-    uploadGivenFileToTelegram "${TS_TAR}"
-fi
-
-rm -f "${TS_TAR}"
+console_print tg "Firmware dump and upload complete!"
+rm -rf ./local_build/
+exit 0
